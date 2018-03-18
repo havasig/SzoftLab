57ca2155f93755ec6c8ab7040301680d1b224760
Jakab <jakab.dominik.b@gmail.com>
2018-03-18 17:28:17 +0100

    Added labels to ctors in Test.

--- test/Logger.java
+++ test/Logger.java
@@ -1,6 +1,10 @@
 import Main.*;
 
-public class Logger {
+/**
+ *
+ */
+public class Logger
+    //A behúzás mértéke, formázás miatt.
     private int tabs;
     //TODO itt kitalálni hogy ez hogy működjön
 
--- test/Test.java
+++ test/Test.java
@@ -31,188 +31,188 @@ public class Test {
     }
 
     public static void MoveToDestination(){
-        Field w_field = new Field();
-        Destination dest = new Destination();
+        Field w_field = new Field("w_field");
+        Destination dest = new Destination("destination");
         w_field.SetNeighbor(Right, dest);
-        Worker worker = new Worker(w_field);
+        Worker worker = new Worker(w_field, "worker");
 
         worker.Control(Right);
     }
 
     public static void MoveToClosedHole(){
-        Field w_field = new Field();
-        Hole hole = new Hole();
+        Field w_field = new Field("w_field");
+        Hole hole = new Hole("hole");
         w_field.SetNeighbor(Right, hole);
-        Worker worker = new Worker(w_field);
+        Worker worker = new Worker(w_field, "worker");
         hole.SetClosed();
 
         worker.Control(Right);
     }
 
     public static void MoveToOpenedHole(){
-        Field w_field = new Field();
-        Hole hole = new Hole();
+        Field w_field = new Field("w_field");
+        Hole hole = new Hole("hole");
         w_field.SetNeighbor(Right, hole);
-        Worker worker = new Worker(w_field);
+        Worker worker = new Worker(w_field, "worker");
         hole.SetOpen();
 
         worker.Control(Right);
     }
 
     public static void MoveToColumn() {
-        Field w_field = new Field();
-        Column column = new Column();
+        Field w_field = new Field("w_field");
+        Column column = new Column("column");
         w_field.SetNeighbor(Right, column);
-        Worker worker = new Worker(w_field);
+        Worker worker = new Worker(w_field, "worker");
 
         worker.Control(Right);
     }
 
     public static void CollideWithWorkerToColumn(){
-        Field w1_field = new Field();
-        Field w2_field = new Field();
-        Column column = new Column();
+        Field w1_field = new Field("w1_field");
+        Field w2_field = new Field("w2_field");
+        Column column = new Column("column");
         w1_field.SetNeighbor(Right, w2_field);
         w2_field.SetNeighbor(Right, column);
-        Worker worker1 = new Worker(w1_field);
-        Worker worker2 = new Worker(w2_field);
+        Worker worker1 = new Worker(w1_field, "worker1");
+        Worker worker2 = new Worker(w2_field, "worker2");
 
         worker1.Control(Right);
     }
 
     public static void CollideWithWorkerToField(){
-        Field w1_field = new Field();
-        Field w2_field = new Field();
-        Field free_field = new Field();
+        Field w1_field = new Field("w1_field");
+        Field w2_field = new Field("w2_field");
+        Field free_field = new Field("free_field");
         w1_field.SetNeighbor(Right, w2_field);
         w2_field.SetNeighbor(Right, free_field);
-        Worker worker1 = new Worker(w1_field);
-        Worker worker2 = new Worker(w2_field);
+        Worker worker1 = new Worker(w1_field, "worker1");
+        Worker worker2 = new Worker(w2_field, "worker2");
 
         worker1.Control(Right);
     }
 
     public static void CollideWithBoxToSwitch(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Switch switcher = new Switch();
-        Hole hole = new Hole();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Switch switcher = new Switch("switcher");
+        Hole hole = new Hole("hole");
         switcher.SetHole(hole);
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, switcher);
-        Worker worker = new Worker(w_field);
-        Box box = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box = new Box(b_field, "box");
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxToWorkerToColumn(){
-        Field w1_field = new Field();
-        Field b1_field = new Field();
-        Field w2_field = new Field();
-        Column column = new Column();
+        Field w1_field = new Field("w1_field");
+        Field b1_field = new Field("b1_field");
+        Field w2_field = new Field("w2_field");
+        Column column = new Column("column");
         w1_field.SetNeighbor(Right, b1_field);
         b1_field.SetNeighbor(Right, w2_field);
         w2_field.SetNeighbor(Right, column);
-        Worker worker1 = new Worker(w1_field);
-        Box box1 = new Box(b1_field);
-        Worker worker2 = new Worker(w2_field);
+        Worker worker1 = new Worker(w1_field, "worker1");
+        Box box1 = new Box(b1_field, "box1");
+        Worker worker2 = new Worker(w2_field, "worker2");
 
         worker1.Control(Right);
     }
 
     public static void CollideWithBoxToWorkerToWorkerToColumn(){
-        Field w1_field = new Field();
-        Field b1_field = new Field();
-        Field w2_field = new Field();
-        Field w3_field = new Field();
-        Column column = new Column();
+        Field w1_field = new Field("w1_field");
+        Field b1_field = new Field("b1_field");
+        Field w2_field = new Field("w2_field");
+        Field w3_field = new Field("w3_field");
+        Column column = new Column("column");
         w1_field.SetNeighbor(Right, b1_field);
         b1_field.SetNeighbor(Right, w2_field);
         w2_field.SetNeighbor(Right, w3_field);
         w3_field.SetNeighbor(Right, column);
-        Worker worker1 = new Worker(w1_field);
-        Box box1 = new Box(b1_field);
-        Worker worker2 = new Worker(w2_field);
-        Worker worker3 = new Worker(w3_field);
+        Worker worker1 = new Worker(w1_field, "worker1");
+        Box box1 = new Box(b1_field, "box1");
+        Worker worker2 = new Worker(w2_field, "worker2");
+        Worker worker3 = new Worker(w3_field, "worker3");
 
         worker1.Control(Right);
     }
 
     public static void CollideWithBoxToField(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Field free_field = new Field();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Field free_field = new Field("free_field");
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, free_field);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(b_field, "box1");
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxToClosedHole(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Hole hole = new Hole();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Hole hole = new Hole("hole");
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, hole);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(b_field, "box1");
         hole.SetClosed();
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxToOpenedHole(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Hole hole = new Hole();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Hole hole = new Hole("hole");
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, hole);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(b_field, "box1");
         hole.SetOpen();
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxToColumn(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Column column = new Column();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Column column = new Column("column");
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, column);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(b_field, "box1");
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxToDestination(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Destination dest = new Destination();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Destination dest = new Destination("destination");
         w_field.SetNeighbor(Right, b_field);
         b_field.SetNeighbor(Right, dest);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(b_field);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(b_field, "box1");
         //Game??
 
         worker.Control(Right);
     }
 
     public static void CollideWithBoxOffFromSwitch(){
-        Field w_field = new Field();
-        Field b_field = new Field();
-        Switch switcher = new Switch();
+        Field w_field = new Field("w_field");
+        Field b_field = new Field("b_field");
+        Switch switcher = new Switch("switcher");
         Field random_field = new Field();
         w_field.SetNeighbor(Right, switcher);
         b_field.SetNeighbor(Right, random_field);
-        Hole hole = new Hole();
+        Hole hole = new Hole("hole");
         switcher.SetHole(hole);
-        Worker worker = new Worker(w_field);
-        Box box1 = new Box(switcher);
+        Worker worker = new Worker(w_field, "worker");
+        Box box1 = new Box(switcher, "box1");
 
         worker.Control(Right);
     }
