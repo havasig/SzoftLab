3d77f9e6ac12baace4f85d46b3d7d098783c014a
gracza <graczadavid@gmail.com>
2018-03-18 17:48:55 +0100

    Added writeTab function

--- test/Logger.java
+++ test/Logger.java
@@ -3,10 +3,17 @@ import Main.*;
 /**
  *
  */
-public class Logger
+public class Logger {
     //A behúzás mértéke, formázás miatt.
     private int tabs;
     //TODO itt kitalálni hogy ez hogy működjön
 
+    private String writeTabs(int num){
+        String temp = "";
+        for(int i = 0; i<num; i++){
+            temp+= "\t";
+        }
+        return temp;
+    }
 
 }
