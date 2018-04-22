66e9a884682928c4100cf254a0d21c081c9d2d29
Gbor <ramacsgabor9@gmail.com>
2018-04-22 21:44:00 +0200

    readmap

--- src/Main/Factory.java
+++ src/Main/Factory.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static Main.Movement.Moved;
 
@@ -79,6 +81,144 @@ public class Factory implements Drawable {
                     getField(x, y).SetNeighbor(Direction.Right, null);
             }
         }
+    }
+
+    public void ReadMap(String file) throws IOException {
+        BufferedReader br = null;
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            line = br.readLine();
+            String[] parts = line.split(" ");
+            for(int i = 0; i < parts.length; i++){
+                if(parts[i].length()!=0)
+                    throw new Error("A tesztesetet nem sikerült futtatni.");
+            }
+            if(parts.length!=2)
+                throw new Error("A tesztesetet nem sikerült futtatni.");
+        int width = Integer.parseInt(parts[0]);
+        int height = Integer.parseInt(parts[1]);
+        this.width = width;
+        this.height = height;
+        GenerateMap(width,height);
+        int actLine = 0;
+        ArrayList<String[]> map = new ArrayList<String[]>();
+        ArrayList<Integer> switchAndHole = new ArrayList<Integer>();
+            while ((line = br.readLine()) != null) {
+                if(actLine<height){
+                    String[] fields = line.split(" ");
+                    if(fields.length!=width)
+                        throw new Error("A tesztesetet nem sikerült futtatni.");
+                    map.add(fields);
+                    actLine++;
+                }
+                    else {
+                    Integer sx = Integer.parseInt(Character.toString(line.charAt(3)));
+                    Integer sy = Integer.parseInt(Character.toString(line.charAt(5)));
+                    Integer hx = Integer.parseInt(Character.toString(line.charAt(7)));
+                    Integer hy = Integer.parseInt(Character.toString(line.charAt(9)));
+
+                    switchAndHole.add(sx);
+                    switchAndHole.add(sy);
+                    switchAndHole.add(hx);
+                    switchAndHole.add(hy);
+                }
+            }
+            for(int i = 0; i < map.size(); i++){
+                for(int j = 0; j < width; j++){
+                    char x = map.get(i)[j].charAt(0);
+                    switch (x){
+                        case 'X':
+                            createColumn(j, i);
+                            break;
+                        case 'D':
+                            createDestination(j, i);
+                            break;
+                        case 'H':
+                            createHole(j, i, HoleState.Open);
+                            break;
+                        case 'h':
+                            createHole(j, i, HoleState.Closed);
+                            break;
+                        case 's':
+                            int index=0;
+                            for(int k=0; k < switchAndHole.size(); k++) {
+                            if (switchAndHole.get(k).equals(j))
+                                if (switchAndHole.get(k + 1).equals(i))
+                                    index=k;
+                            }
+                            if(index==0)
+                                throw new Error("A tesztesetet nem sikerült futtatni.");
+                            createSwitch(j,i,switchAndHole.get(index+2), switchAndHole.get(index+3));
+                            break;
+                        case 'S':
+                            int indexS=0;
+                            for(int k=0; k < switchAndHole.size(); k++) {
+                                if (switchAndHole.get(k).equals(j))
+                                    if (switchAndHole.get(k + 1).equals(i))
+                                        index=k;
+                            }
+                            if(indexS==0)
+                                throw new Error("A tesztesetet nem sikerült futtatni.");
+                            createSwitch(j,i,switchAndHole.get(indexS+2), switchAndHole.get(indexS+3));
+                            break;
+                        case '_':
+                            break;
+                        default:
+                            new Error("A tesztesetet nem sikerült futtatni.");
+                    }
+                    char y = map.get(i)[j].charAt(1);
+                    switch (y){
+                        case 'B':
+                            addBox(j,i);
+                            break;
+                        case '1':
+                            addWorker(j, i,1);
+                            break;
+                        case '2':
+                            addWorker(j, i,2);
+                            break;
+                        case '3':
+                            addWorker(j, i,3);
+                            break;
+                        case '4':
+                            addWorker(j, i,4);
+                            break;
+                        case '5':
+                            addWorker(j, i,5);
+                            break;
+                        case '6':
+                            addWorker(j, i,6);
+                            break;
+                        case '7':
+                            addWorker(j, i,7);
+                            break;
+                        case '8':
+                            addWorker(j, i,8);
+                            break;
+                        case '9':
+                            addWorker(j, i,9);
+                            break;
+                        case '_':
+                            break;
+                        default:
+                            new Error("A tesztesetet nem sikerült futtatni.");
+                    }
+                    char z = map.get(i)[j].charAt(2);
+                    switch (z){
+                        case 'M':
+                            getField(j,i).setSplich(Field.FieldState.Honey);
+                            break;
+                        case 'O':
+                            getField(j,i).setSplich(Field.FieldState.Oil);
+                            break;
+                        case '_':
+                            break;
+                        default:
+                            new Error("A tesztesetet nem sikerült futtatni.");
+                    }
+                }
+            }
+
     }
 
 
